b464932b3e0c4a8c0a68a72abbbe127d35fcb33f
tennirb <gabriel.castro@pioneiro.g12.br>
2025-04-16 12:37:06 -0300

    coloquei as arvores e alguns arbustos

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -8,11 +8,27 @@ namespace myTiles {
     export const tile2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level":
-            case "level2":return tiles.createTilemap(hex`1000100003030303030303030303030303030303030303030303030303030303030303030301010103030303030303030303030303030101030303030303030303030303030303030303030302030303030303030303030303030303030203030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030202030303030303030303030303030302030303030303030103030303030303030303030303030301010303030303030303030303030303030301010303030303030303030303030303030303030303030303030303030303030303030303030303030303`, img`
+            case "level2":return tiles.createTilemap(hex`1000100003030303030303030303030303030303030303030303030303030504030303030301010103030303030303030303030303030101030303030303030303030303030303030303030302030303030303030303030303030303030203030303030303050403030303030504030303030303030303030303030305040303030303030303030303030303060703030303030303030303030303030809030202030303030303030303030303030302030303030303030103030303030303030303030303030301010303030303030303030303030303030301010303030303030303030303030303030303030303030303030303030303030303030303030303030303`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -29,7 +45,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.castle.tileGrass2,myTiles.tile3], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.castle.tileGrass2,myTiles.tile3,myTiles.tile11,myTiles.tile10,myTiles.tile9,myTiles.tile8,myTiles.tile7,myTiles.tile4], TileScale.Sixteen);
         }
         return null;
     })
@@ -43,6 +59,22 @@ namespace myTiles {
             case "tile2":return tile2;
             case "myTile1":
             case "tile3":return tile3;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile2":
+            case "tile4":return tile4;
         }
         return null;
     })
